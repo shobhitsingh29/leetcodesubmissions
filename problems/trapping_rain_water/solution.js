164448d3c393d7d240eda4e504aa591cbe4787a2
shobhit singh <shobhitsingh29@gmail.com>
2019-12-10 20:21:49 +0000

    Sync LeetCode submission - Trapping Rain Water (javascript)

---
+++ problems/trapping_rain_water/solution.js
@@ -0,0 +1,21 @@
+function trap(height) {
+  if (height == null || height.length === 0) return 0;
+
+  let res = 0;
+  for (let i = 0; i < height.length; i++) {
+    let lMax = 0;
+    let rMax = 0;
+
+    for (let j = 0; j < i; j++) {
+      lMax = Math.max(lMax, height[j]);
+    }
+    for (let j = i + 1; j < height.length; j++) {
+      rMax = Math.max(rMax, height[j]);
+    }
+
+    const water = Math.min(lMax, rMax) - height[i];
+    if (water > 0) res += water;
+  }
+
+  return res;
+}
