37f4f464a16ed06a3adc7fba1f4336c763c50400
shobhit singh <shobhitsingh29@gmail.com>
2020-05-09 14:30:52 +0000

    Sync LeetCode submission - Valid Perfect Square (javascript)

---
+++ problems/valid_perfect_square/solution.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    if( !Math.sqrt(num).toString().includes(".")){
+        return true;
+    }
+    return false
+    
+};
