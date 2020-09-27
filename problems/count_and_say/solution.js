dd517a590755a4c545fb139102e0e9d3e6372413
shobhit singh <shobhitsingh29@gmail.com>
2020-09-27 16:50:51 +0000

    Sync LeetCode submission - Count and Say (javascript)

---
+++ problems/count_and_say/solution.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    
+    if (n <= 0)
+        return null;
+ 
+    var result = "1";
+    var i = 1;
+ 
+    while (i < n) {
+        var sb = '';
+        var count = 1;
+        for (var j = 1; j < result.length; j++) {
+            if (result.charAt(j) == result.charAt(j - 1)) {
+                count++;
+            } else {
+                sb += count;
+                sb += result.charAt(j - 1);
+                count = 1;
+            }
+        }
+ 
+        sb += count;
+        sb += result.charAt(result.length - 1);
+        result = sb;
+        i++;
+    }
+ 
+    return result;
+};
