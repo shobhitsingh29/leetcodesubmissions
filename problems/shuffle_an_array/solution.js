81663f70d3448bc40834b110fad140ce39f92c43
shobhit singh <shobhitsingh29@gmail.com>
2020-10-06 17:43:21 +0000

    Sync LeetCode submission - Shuffle an Array (javascript)

---
+++ problems/shuffle_an_array/solution.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} nums
+ */
+var Solution = function(nums) {
+    this.nums=[...nums];
+    this.array=[...nums];
+    
+};
+
+/**
+ * Resets the array to its original configuration and return it.
+ * @return {number[]}
+ */
+Solution.prototype.reset = function() {
+    return this.nums;
+};
+
+/**
+ * Returns a random shuffling of the array.
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function() {
+    var currentIndex = this.array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = this.array[currentIndex];
+    this.array[currentIndex] = this.array[randomIndex];
+    this.array[randomIndex] = temporaryValue;
+  }
+
+  return this.array;
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
