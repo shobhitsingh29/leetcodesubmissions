b36421a386729962d4df286f4e4be0f0e860e596
shobhit singh <shobhitsingh29@gmail.com>
2020-10-11 10:54:40 +0000

    Sync LeetCode submission - Min Stack (javascript)

---
+++ problems/min_stack/solution.js
@@ -0,0 +1,40 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+    
+    this.stack=[];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    
+    this.stack.push(x);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    
+    this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length-1]
+    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+   let min = Math.min(...this.stack)
+return min
+};
