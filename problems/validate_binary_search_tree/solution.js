1d32abacaf68c3e200bee7732380f19b010d6989
shobhit singh <shobhitsingh29@gmail.com>
2021-02-12 21:47:51 +0000

    Sync LeetCode submission - Validate Binary Search Tree (javascript)

---
+++ problems/validate_binary_search_tree/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var validate=function( root,  low,  high){
+    if (root == null) {
+            return true;
+        }
+     if ((low != null && root.val <= low) || (high != null && root.val >= high)) {
+            return false;
+        }
+        // The left and right subtree must also be valid.
+        return validate(root.right, root.val, high) && validate(root.left, low, root.val);
+    
+}
+var isValidBST = function(root) {
+      return validate(root, null, null);
+};
