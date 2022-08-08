8c9e0e38135ce4d4ca764444545101a4044de630
shobhit singh <shobhitsingh29@gmail.com>
2022-08-08 00:17:02 +0000

    Sync LeetCode submission - Isomorphic Strings (javascript)

--- problems/isomorphic_strings/solution.js
+++ problems/isomorphic_strings/solution.js
@@ -1,53 +1,11 @@
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
 var isIsomorphic = function(s, t) {
-   if(s=="bbbaaaba" && t=='aaabbbba'){
-       return false
-   }
-    if(s=="abba" && t=='abab'){
-       return false
-   }
-    
-   var  freqMap1=frqmap(s);
-    var freqMap2=frqmap(t);
-    freqMap1=Object.values(freqMap1);
-    freqMap2=Object.values(freqMap2);
-    console.log(freqMap2,freqMap1)
-    var res=false;
-    var res1=true;
-    if(freqMap1.length===freqMap1.length){
-        for(let i=0;i<freqMap1.length;i++){
-            
-            if(freqMap1[i]===freqMap2[i]){
-               continue;
-               }else{
-                res1=false;
-                break;
-            }
+    const dict = {}
+    for (let i = 0; i < s.length; i++) {
+        if (!dict[s[i]] && !Object.values(dict).includes(t[i])) {
+            dict[s[i]] = t[i]
+        } else if (dict[s[i]] !== t[i]) {
+            return false;
         }
-        
     }
-    console.log(res1,"**")
-    if(res1){
-       return true; 
-    }
-    return res;
-    
+    return true;
 };
-
-function frqmap(s){
-    var freqMap1={};
-    [...s].map(i=>{
-        if(!freqMap1[i]){
-            freqMap1[i]=1;
-        }else{
-            freqMap1[i]++;
-        }
-    })
-    return freqMap1;
-    
-    
-}
