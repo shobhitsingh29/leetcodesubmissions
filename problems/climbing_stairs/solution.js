7086e1b9293fae4482692ba6b5b797063cd60b9e
shobhit singh <shobhitsingh29@gmail.com>
2023-04-08 17:12:23 +0000

    Sync LeetCode submission - Climbing Stairs (javascript)

--- problems/climbing_stairs/solution.js
+++ problems/climbing_stairs/solution.js
@@ -1,6 +1,22 @@
-const res={};
-const climbStairs = function(n) {
-     var sqrt5=Math.sqrt(5);
-        var fibn=Math.pow((1+sqrt5)/2,n+1)-Math.pow((1-sqrt5)/2,n+1);
-        return parseInt(fibn/sqrt5);
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ const dp=[0,1,2,3,5,8];
+
+var climbStairs = function(n) {
+    
+if(n==2){
+    return 2;
+}
+if(n==1){
+    return 1;
+}
+if(dp[n]){
+    return dp[n]
+}
+dp[n]=climbStairs(n-1)+climbStairs(n-2)
+return dp[n];
+
+
 };
